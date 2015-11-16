ef63d1a13fc19c0bd526ea52e9a5f44a8924a000
rhpark <rhpark@tmon.co.kr>
2015-11-16 18:30:29 +0900

    1. 파이어베이스 적용 2. imgur 삭제

---
+++ app/src/main/java/com/pockru/firebase/UrlConstants.java
@@ -0,0 +1,14 @@
+package com.pockru.firebase;
+
+/**
+ * Created by rhpark on 2015. 11. 16..
+ * JIRA: MWP-
+ */
+public class UrlConstants {
+    public static final String FIREBASE_URL = "https://bestizbox.firebaseio.com";
+
+    public static final String FIREBASE_CONNECTED = ".info/connected";
+    public static final String FIREBASE_CHAT = "chat";
+    public static final String FIREBASE_CHAT_SEND = "";
+    public static final String FIREBASE_CHAT_RECEIVE = "";
+}
